b1e3ff52f3450c1a85b720c797467242cff92085
HP_pellel <algassimoupelleldiallo2005@gmail.com>
2024-09-25 13:59:31 +0200

    Votre message de commit ici

---
+++ TP2dev/tp.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(void){
+// TP03 sur les caractère
+/*printf("TP sur les caractères: \n");
+printf("%c%c%c%c%c%c%c%c%c%c%c%c%c",'H','e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!', '\n');
+
+printf("%c\n", '6' + 1);
+
+printf("%c\n", 'a' - 0b100000);
+
+printf("Unicode : %c%c\n", '\xC3', '\xAE');
+printf("Latin 1 : %c\n", '\xEE');
+
+printf("%c\n", 'î');
+
+char genre;
+char annee1;
+char annee2; 
+char mois1;
+char mois2;
+genre = getchar();
+annee1 = getchar();
+annee2 = getchar();
+mois1 = getchar();
+mois2 = getchar();
+
+printf("Mois et année de naissance : %c%c%c%c%c\n", mois1, mois2, '/',annee1, annee2);*/
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// TP05 sur les Réels
+/*printf("%f\n", 12345.678910111213);
+printf("%.12f\n", 12345.678910111213);
+printf("%.15f\n", 12345.678910111213);*/
+
+/*printf("%f\n", 5.0+2.5);
+printf("%f\n", 5.0-2.5);
+printf("%f\n", 5.0*2.5);
+printf("%f\n", 5.0/2.5);
+printf("%f\n", 5.0%2.5); --> le modulo ne fonctionne pas avec les flotants car la division n'a pas de reste */
+
+/*printf("%f\n", +1.0/0.0); --> +inf
+printf("%f\n", -1.0/0.0); --> -inf
+printf("%f\n", -0.0/0.0); --> nan parce que ça n'existe pas */
+
+
+
+return 0;		
+}
+
+
+
